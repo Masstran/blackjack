f4f595de34f2bc66876100e27bc771edf5abfc21
Masstran <15689794+Masstran@users.noreply.github.com>
2021-09-10 21:12:22 +0000

    Some output modifications

--- main.py
+++ main.py
@@ -26,18 +26,25 @@ def cls():
   """ Cross-platform clear screen """
   os.system('cls' if os.name == 'nt' else 'clear')
 
+def reset_screen():
+  cls()
+  print(logo)
+
 def check_yn_input(text: str):
   while text not in ['y', 'n']:
-    text = input("Invalid input! 'y' or 'n' only!\n").lower()
+    text = input("Invalid input! 'y' or 'n' only! ").lower()
   return text
 
-print(logo)
-start_play = input("You wanna play some blackjack? 'y' or 'n'.\n").lower()
+reset_screen()
+
+
+start_play = input("You wanna play some blackjack? 'y' or 'n'. ").lower()
 
 start_play = check_yn_input(start_play)
 
-if start_play == 'n':
-  exit()
+# Should we even play?
+keep_playing = start_play == 'y'
+
 
 ace_name = "A"
 card_value = {
@@ -86,22 +93,24 @@ def print_game_info(full: bool = False):
   """Prints current state of the game"""
   dealer_print = dealer_hand if full else dealer_hand[0]
   dealer_start_text = "Dealer's cards" if full else "Dealer's first card"
-  print(f"{dealer_start_text}: {dealer_print} ({dealer_score})")
-  print(f"Your cards: {player_hand} ({player_score})")
+  dealer_local_score = dealer_score if full else card_value[dealer_hand[0]]
+  print(f"\tYour cards: {player_hand} ({player_score})")
+  print(f"\t{dealer_start_text}: {dealer_print} ({dealer_local_score})")
 
-keep_playing = True
+
 
 def play_again():
   """Asks player if he wants to play again"""
   global keep_playing
-  play = input("Do you wish to play again? 'y' or 'n'\n").lower()
+  play = input("Do you wish to play again? 'y' or 'n' ").lower()
   play = check_yn_input(play)
   if play == 'n':
     keep_playing = False
 
 while keep_playing:
-  cls()
-  print(logo)
+  reset_screen()
+  print("LET US BEGIN!")
+
   player_hand.clear()
   dealer_hand.clear()
   player_score = 0
@@ -115,53 +124,52 @@ while keep_playing:
   deal_card(dealer_hand)
 
   player_score = calculate_score(player_hand)
-  dealer_score = card_value[dealer_hand[0]]
+  dealer_score = calculate_score(dealer_hand)
 
   keep_drawing = True
+
   # 1st part: player draws cards
   while keep_drawing and player_score <= 21:
     print_game_info()
 
-    wanna_draw = input("Do you want to draw an extra card? 'y' or 'n'\n").lower()
+    wanna_draw = input("Type 'y' to draw a card or 'n' to stop. ").lower()
     while wanna_draw not in ['y', 'n']:
       wanna_draw = check_yn_input(wanna_draw)
 
     if wanna_draw == 'y':
       new_card = deal_card(player_hand)
-      print(f"You got new card: {new_card}")
       player_score = calculate_score(player_hand)
     else:
       keep_drawing = False
 
   # 2nd part: check if player is still in the game
 
-  if player_score > 21:
-    print_game_info()
-    print("I'm sorry, but you lose it all. Bye bye")
-    play_again()
-    continue
+  # if player_score > 21:
+  #   print_game_info()
+  #   print("I'm sorry, but you lose it all. Bye bye")
+  #   play_again()
+  #   continue
   
   # 3rd part: get dealer cards
+  if player_score <= 21:
 
-  dealer_score = calculate_score(dealer_hand)
-
-  while dealer_score < 17:
-    print_game_info(full=True)
-    new_card = deal_card(dealer_hand)
-    print(f"Dealer got a new card: {new_card}")
-    dealer_score = calculate_score(dealer_hand)
+    while dealer_score < 17 and dealer_score < player_score:
+      # print_game_info(full=True)
+      new_card = deal_card(dealer_hand)
+      # print(f"Dealer got a new card: {new_card}")
+      dealer_score = calculate_score(dealer_hand)
 
   # 4th part: find the winner
 
   print_game_info(full=True)
 
-  if dealer_score > 21 or player_score > dealer_score:
-    print("Hey! You win, who would have guessed!")
+  if player_score > 21 or (dealer_score <= 21 and dealer_score > player_score):
+    print("Well, you lose. Deal with it.")
   elif player_score == dealer_score:
     print("It's a draw!")
   else:
-    print("Well, you lost. Deal with it.")
-
+    print("Hey! You win, who would have guessed!")
+    
   play_again()
 
 cls()
